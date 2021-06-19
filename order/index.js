743c8ff9cfc559b175cdefd8d144e5cfb8e703c5
Tanawat Tassana <amp.tanawat@gmail.com>
2021-06-19 14:54:16 +0700

    Send message to Kafka from fastify

--- order/index.js
+++ order/index.js
@@ -1,29 +1,37 @@
 const { Kafka } = require("kafkajs");
+const fastify = require("fastify");
 
 const kafka = new Kafka({
   clientId: "order-client",
   brokers: ["broker0:9092", "broker1:9092"],
 });
-
 const producer = kafka.producer();
 
-const run = async () => {
-  console.log("️connecting to Kafka");
-  await producer.connect();
-  console.log("✅ connected");
-  console.log("try sending the message...");
+const app = fastify({ logger: true });
+app.post("/order/:order_id", async (request, reply) => {
+  const orderId = request.params.order_id;
+  console.log("received order with id:", orderId);
   await producer.send({
     topic: "order_received",
     messages: [
       {
         value: JSON.stringify({
-          order_id: "1234",
+          order_id: orderId,
           received_at: `${new Date().valueOf()}`,
         }),
       },
     ],
   });
-  console.log("✉️ order_received message sent");
+  console.log("✉️ Kafka:order_received message sent");
+  reply.code(200);
+  return "";
+});
+
+const run = async () => {
+  await producer.connect();
+  console.log("✅ Kafka connected");
+  await app.listen(80, "0.0.0.0");
+  console.log("🧸 Order service started");
 };
 
 run().catch(console.error);
